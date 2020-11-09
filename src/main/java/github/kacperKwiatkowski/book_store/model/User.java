cd161078723b2cfa4124392787660e119579f494
Kacper Kwiatkowski <kacper18k@gmail.com>
2020-11-09 22:10:05 +0100

    User model improved.

--- src/main/java/github/kacperKwiatkowski/book_store/model/User.java
+++ src/main/java/github/kacperKwiatkowski/book_store/model/User.java
@@ -3,6 +3,7 @@ package github.kacperKwiatkowski.book_store.model;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "users")
@@ -10,4 +11,16 @@ public class User {
 
     @Id
     private int id;
+    private String email;
+    private String nick;
+    private String password;
+    private String firstname;
+    private String lastname;
+    private String phoneNum;
+    private int houseNum;
+    private int flatNum;
+    private int street;
+    private String town;
+    private String postcode;
+    private LocalDate dob;
 }
